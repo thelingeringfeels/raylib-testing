5722cc534f764eb3ed233fcce05359a39f67a6f8
R. Farris <casualenglishmajour@gmail.com>
2026-06-02 00:06:08 -0600

    HOLY MOLY I DID IT I DID POINTER LOL THIS IS WONDERFUL

--- raylib-test.c
+++ raylib-test.c
@@ -1,39 +1,51 @@
 #include "include/raylib.h"
 #include <math.h>
 
+typedef struct{
+	double posX;
+	double posY;
+} Pos;
+
+void changePos(Pos *pos);
+
 int main(void)
 {
 	const int screenWidth = 900;
 	const int screenHeight = 600;
 	InitWindow(screenWidth, screenHeight, "New Window");
 
-	double posX = 0;
-	int posY = 0;
+	Pos pos;
+	pos.posX = 0;
+	pos.posY = 0;
 	
 	SetTargetFPS(60);
 	
 	while(!WindowShouldClose())
 	{
 		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-			posX = 0;
-			posY = 300;
+			pos.posX = 0;
+			pos.posY = 300;
 		}
 		
-		posX+=GetFrameTime()*150;
-		if(posX > 900){
-			posX=-10;
-		}
-
-		posY = sin(posX/50)*100 + 300;
+		changePos(&pos);
 
 		BeginDrawing();
 			ClearBackground(LIGHTGRAY);
 
-			DrawRectangle(posX, posY, 10, 10, BLACK);
+			DrawRectangle(pos.posX, pos.posY, 10, 10, BLACK);
 		EndDrawing();
 	}
 	
 	CloseWindow();
 	
 	return 0;
+}
+
+void changePos(Pos *pos){
+	pos->posX+=GetFrameTime()*150;
+	if(pos->posX > 900){
+		pos->posX=-10;
+	}
+
+	pos->posY = sin(pos->posX/50)*100 + 300;
 }
