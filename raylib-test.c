8e6f655b7995de48f49570b69a0aba2cc075e83b
R. Farris <casualenglishmajour@gmail.com>
2026-06-02 01:12:07 -0600

    Just wrapping up this little intro of mine into raylib

--- raylib-test.c
+++ raylib-test.c
@@ -1,50 +1,85 @@
 #include "include/raylib.h"
 #include <math.h>
 
+#define SWIDTH 900
+#define SHEIGHT 600
+
+#define SFPS 60
+
 typedef struct{
 	double posX;
 	double posY;
 } Pos;
 
-void changePos(Pos *pos);
+void changePos(Pos *pos, Texture2D sprite);
 
 int main(void)
 {
-	const int screenWidth = 900;
-	const int screenHeight = 600;
-	InitWindow(screenWidth, screenHeight, "New Window");
+	Texture2D sprite;
+	Sound sound;
+	Music music;
+	
+	InitWindow(SWIDTH, SHEIGHT, "New Window");
+	
+	InitAudioDevice();
+	
+	sprite = LoadTexture("junk/trngls.png");
+	sound = LoadSound("junk/freesound_community-gunshot-37055.mp3");
+	music = LoadMusicStream("junk/edelweiss.mp3");
 
 	Pos pos;
 	pos.posX = 0;
 	pos.posY = 0;
 	
-	SetTargetFPS(60);
+	SetTargetFPS(SFPS);
 	
 	while(!WindowShouldClose())
 	{
-		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-			pos.posX = 0;
-			pos.posY = 300;
+		UpdateMusicStream(music);
+		
+		changePos(&pos, sprite);
+		
+		if(IsKeyPressed(KEY_SPACE))
+		{
+			StopMusicStream(music);
+			PlayMusicStream(music);
 		}
 		
-		changePos(&pos);
+		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+		{
+			Rectangle spriteRect = {
+			pos.posX,
+			pos.posY,
+			sprite.width,
+			sprite.height
+			};
+			
+			if(CheckCollisionPointRec(GetMousePosition(), spriteRect))
+			{
+				PlaySound(sound);	
+			}
+		}
 
 		BeginDrawing();
 			ClearBackground(LIGHTGRAY);
 
-			DrawRectangle(pos.posX, pos.posY, 10, 10, BLACK);
+			DrawTexture(sprite, pos.posX, pos.posY, LIGHTGRAY);
 		EndDrawing();
 	}
+	
+	StopMusicStream(music);
+	
+	CloseAudioDevice();
 	
 	CloseWindow();
 	
 	return 0;
 }
 
-void changePos(Pos *pos){
+void changePos(Pos *pos, Texture2D sprite){
 	pos->posX+=GetFrameTime()*150;
 	if(pos->posX > 900){
-		pos->posX=-10;
+		pos->posX=-sprite.width;
 	}
 
 	pos->posY = sin(pos->posX/50)*100 + 300;
